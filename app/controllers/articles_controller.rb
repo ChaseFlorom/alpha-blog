e7dabed1356f9cac29c8b24cb08842cca64cc945
florodude <florodude@hotmail.com>
2019-11-01 20:40:13 +0000

    Added Article show capacity

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,13 +4,24 @@ class ArticlesController < ApplicationController
     end
   
     def create
-        render plain: params[:article].inspect
+        #render plain: params[:article].inspect
         @article = Article.new(article_params)
         @article.save
-        redirect_to articles_show(@article)
         
+        #Checking to make sure validations go through
+        if @article.save 
+            flash[:notice] = "The article was successfully submitted."
+            redirect_to article_path(@article)
+        else
+
+            render 'new'
+        end
         
     end 
+    
+    def show 
+        @article = Article.find(params[:id])
+    end
     
     private 
     def article_params
